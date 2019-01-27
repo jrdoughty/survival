09fded35af230057e681925601f542790967ba43
John Doughty <jrdoughty13@gmail.com>
2019-01-27 15:34:30 -0600

    bugs bugs bugs!

--- src/states/RestState.hx
+++ src/states/RestState.hx
@@ -40,7 +40,7 @@ class RestState extends BaseState
         t.color.setColor(Reg.buttonTextColor);
         
         var b = new Button(this,5, 200, 100, 34, new Bitmap(Reg.images[Data.config.all[0].buttonBG].toTile()),'Excercise\n\r-10 Anxiety', function(_){Reg.anxiety-=10;pick();}, 18);
-        b.overDelegate = function (){t.text = 'Sweatin\' out the shakes settles ones nerves. ';}
+        b.overDelegate = function (){t.text = 'Sweatin\' out the shakes settles one\'s nerves. ';}
         b = new Button(this,110, 200, 100, 34, new Bitmap(Reg.images[Data.config.all[0].buttonBG].toTile()),'Read\n\r-10 Depression', function(_){Reg.depression-=10;pick();}, 18);
         b.overDelegate = function (){t.text = 'Read about fronteirsmen... Makes this bein\' lost feel like it has a purpose.';}
         b = new Button(this,215, 200, 100, 34, new Bitmap(Reg.images[Data.config.all[0].buttonBG].toTile()),'Sleep\n\r-10 Exhaustion', function(_){Reg.exhaustion-=10;pick();}, 18);
